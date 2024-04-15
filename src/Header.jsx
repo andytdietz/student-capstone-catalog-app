263285f744c854682d9d59ab16c630e3de65730f
matthewdillender <156735514+matthewdillender@users.noreply.github.com>
2024-04-15 10:36:52 -0700

    navbar

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,20 @@
+import React from "react";
+
 export function Header() {
   return (
     <header>
-      <nav>
-        <a href="#">Home</a> | <a href="#">Link</a>
+      {/* Navbar with Bootstrap classes */}
+      <nav className="navbar navbar-light bg-light justify-content-between">
+        <a className="navbar-brand">Navbar</a>
+        {/* Form for searching */}
+        <form className="form-inline">
+          {/* Search input field */}
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          {/* Search button */}
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
       </nav>
     </header>
   );
