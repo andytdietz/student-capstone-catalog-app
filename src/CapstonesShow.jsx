204624c1a2b1f32c53a93ee2e018a30827f29e8e
andytdietz <andytdietz@gmail.com>
2024-04-15 12:05:43 -0600

    show action

---
+++ src/CapstonesShow.jsx
@@ -0,0 +1,17 @@
+export function CapstonesShow() {
+  return (
+    <div id="capstones-show">
+      <h1>Andy Dietz</h1>
+      <p>Project Name: Fitness Tracking App</p>
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/000/435/420/original/healthy-lifestyle-iconset-for-fitness-app-vector.jpg"
+        alt=""
+      />
+      <p>
+        Description: Andy Dietz&apos;s capstone project is a Fitness Tracking App, dedicated to helping users achieve
+        their fitness goals. With features like workout tracking, meal planning, and progress visualization, it provides
+        comprehensive support for a healthy lifestyle.
+      </p>
+    </div>
+  );
+}
